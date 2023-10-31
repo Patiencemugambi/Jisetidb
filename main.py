51339af87bd5c76aae83fcf13d3210b9d1a74be9
kibetbrian <brian.kibet2@student.moringaschool.com>
2023-10-31 20:11:15 +0300

    created the routes

---
+++ main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import models, schemas, database
+from typing import List
+from pydantic import BaseModel
+from app.schemas import Status
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI()
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = models.User(username=user.username, email=user.email, role=user.role)
+    db_user.password = user.password
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.put("/users/{user_id}", response_model=schemas.User)
+def update_user(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    for field, value in user.dict().items():
+        setattr(db_user, field, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(db_user)
+    db.commit()
+    return {"message": "User deleted"}
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = db.query(models.User).offset(skip).limit(limit).all()
+    return users
+
+@app.get("/statuses/", response_model=List[schemas.Status])
+def read_statuses(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    statuses = db.query(models.Status).offset(skip).limit(limit).all()
+    return statuses
+
+@app.post("/statuses/", response_model=schemas.Status)
+def create_status(status: schemas.StatusCreate, db: Session = Depends(get_db)):
+    db_status = models.Status(name=status.name)
+    db.add(db_status)
+    db.commit()
+    db.refresh(db_status)
+    return db_status
